b91b8a26416cefa7180481fa9e61090d702a475a
imKaustubh29 <51872311+imKaustubh29@users.noreply.github.com>
2024-05-23 18:07:36 +0200

    Update main.cpp

--- 01_OBJ/main.cpp
+++ 01_OBJ/main.cpp
@@ -4,60 +4,63 @@
 
 class MyGLApp : public GLApp {
 public:
-  double angle{0.0};
-  std::vector<float> data;
-  
-  MyGLApp() : GLApp{640,480,1,"Shared vertices to explicit representation demo"} {}
-  
-  virtual void init() override {
-    GL(glDisable(GL_CULL_FACE));
-    GL(glEnable(GL_DEPTH_TEST));
-    GL(glClearColor(0,0,0,0));
+    double angle{ 0.0 };
+    std::vector<float> data;
+    std::vector<float> data_cube;
+    bool bCube{ false };
 
-    const OBJFile m{"bunny.obj", true};
+    MyGLApp() : GLApp{ 640,480,1,"Shared vertices to explicit representation demo" } {}
 
-    // TODO:
-    // Replace this example block of code  by your code to
-    // convert the shared vertex representation in the object m
-    // by an explicit representation, indices are stored in
-    // the stl-vector m.indices the vertex positions are stored
-    // in m.vertices and the normals are stored in m.normals.
-    // As color you can choose whatever you like
-    
-    data.push_back(0.0f); data.push_back(0.5f); data.push_back(0.0f);  // position
-    data.push_back(1.0f); data.push_back(0.0f); data.push_back(0.0f); data.push_back(1.0f); // color
-    data.push_back(0.0f); data.push_back(0.0f); data.push_back(1.0f); // normal
+    virtual void init() override {
+        GL(glDisable(GL_CULL_FACE));
+        GL(glEnable(GL_DEPTH_TEST));
+        GL(glClearColor(0, 0, 0, 0));
 
-    data.push_back(-0.5f); data.push_back(-0.5f); data.push_back(0.0f);
-    data.push_back(0.0f); data.push_back(0.0f); data.push_back(1.0f); data.push_back(1.0f);
-    data.push_back(0.0f); data.push_back(0.0f); data.push_back(1.0f);
+        data = loadOBJ("bunny.obj");
+        data_cube = loadOBJ("cube.obj");
+    }
 
-    data.push_back(0.5f); data.push_back(-0.5f); data.push_back(0.0f);
-    data.push_back(0.0f); data.push_back(1.0f); data.push_back(0.0f); data.push_back(1.0f);
-    data.push_back(0.0f); data.push_back(0.0f); data.push_back(1.0f);
+    virtual void animate(double animationTime) override {
+        angle = animationTime * 30;
+    }
 
-    // example block end
-  }
-  
-  virtual void animate(double animationTime) override {
-    angle = animationTime*30;
-  }
-  
-  virtual void draw() override {
-    GL(glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT));
-    setDrawProjection(Mat4::perspective(45, glEnv.getFramebufferSize().aspect(), 0.0001f, 100.0f));
-    setDrawTransform(Mat4::lookAt({0,0,2},{0,0,0},{0,1,0}) * Mat4::rotationY(float(angle)));
-    drawTriangles(data, TrisDrawType::LIST, false, true);
-  }
-  
-  virtual void keyboardChar(unsigned int key) override {
-    // TODO check for keystrokes here
-  }
+    virtual void draw() override {
+        GL(glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
+        setDrawProjection(Mat4::perspective(45, glEnv.getFramebufferSize().aspect(), 0.0001, 100));
+        setDrawTransform(Mat4::lookAt({ 0,0,2 }, { 0,0,0 }, { 0,1,0 }) * Mat4::rotationY(float(angle)));
+        drawTriangles(bCube ? data_cube : data, TrisDrawType::LIST, false, true);
+    }
 
+    virtual void keyboardChar(unsigned int key) override {
+        if (key == GLFW_KEY_SPACE) bCube = !bCube;
+    }
+
+private:
+    std::vector<float> loadOBJ(const std::string& filename) {
+        std::vector<float> target;
+        const OBJFile m{ filename, true };
+        for (const OBJFile::IndexType& triangle : m.indices) {
+            for (const size_t& index : triangle) {
+                target.push_back(m.vertices[index][0]);
+                target.push_back(m.vertices[index][1]);
+                target.push_back(m.vertices[index][2]);
+
+                target.push_back(m.vertices[index][0] + 0.5f);
+                target.push_back(m.vertices[index][1] + 0.5f);
+                target.push_back(m.vertices[index][2] + 0.5f);
+                target.push_back(1.0f);
+
+                target.push_back(m.normals[index][0]);
+                target.push_back(m.normals[index][1]);
+                target.push_back(m.normals[index][2]);
+            }
+        }
+        return target;
+    }
 
 } myApp;
 
-int main(int argc, char ** argv) {
-  myApp.run();
-  return EXIT_SUCCESS;
+int main(int argc, char** argv) {
+    myApp.run();
+    return EXIT_SUCCESS;
 }
